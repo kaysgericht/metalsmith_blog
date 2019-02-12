01856e475fca5a1b3055e5c7d0d42723a464651f
Thorsten <thorsten.meister@bbv.ch>
2019-02-12 11:01:01 +0100

    serve and watch

--- build.js
+++ build.js
@@ -6,6 +6,8 @@ var collections = require('metalsmith-collections');
 var discoverPartials = require('metalsmith-discover-partials');
 var renameme = require('metalsmith-rename');
 var permalinks = require('metalsmith-permalinks');
+var serve = require('metalsmith-serve');
+var watch = require('metalsmith-watch');
 
 metalsmith(__dirname)
   .metadata({
@@ -39,7 +41,16 @@ metalsmith(__dirname)
     pattern: ':title',
   }))
   .use(layouts())
- 
+  .use(serve({
+    port: 8081,
+    verbose: true
+  }))
+  .use(watch({
+      paths: {
+        "${source}/**/*": true,
+        "layouts/**/*": "**/*",
+      }
+    }))
   .build(function (err) {
     if (err) {
       console.log(err);
